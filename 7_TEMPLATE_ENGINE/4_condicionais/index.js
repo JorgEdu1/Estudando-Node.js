e28049351920f1fcb31f207a2163b8868adb241d
JorgEdu1 <jorge.eduardo@alu.ufc.br>
2023-10-09 19:24:56 -0300

    condicionais

---
+++ 7_TEMPLATE_ENGINE/4_condicionais/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+
+app.get('/', (req, res) => {
+
+    const user = {
+        name: 'John',
+        surname: 'Doe',
+    };
+
+    const auth = false;
+
+    res.render('home',{user: user, auth});
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
