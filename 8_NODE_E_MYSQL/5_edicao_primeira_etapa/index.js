092a988e501986701cd51f460f1bb5cb6f513a45
JorgEdu1 <jorge.eduardo@alu.ufc.br>
2024-01-03 14:29:04 -0300

    resgatando dados para editar posteriormente

---
+++ 8_NODE_E_MYSQL/5_edicao_primeira_etapa/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const query = `INSERT INTO books (title, page) VALUES ('${title}', '${pageqty}')`;  
+
+    conn.query(query, (err, result) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        res.redirect('/books');
+    });
+});
+
+app.get('/books', (req, res) => {
+    const query = 'SELECT * FROM books';
+
+    conn.query(query, (err, result) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        res.render('books', { books: result });
+    });
+});
+
+app.get('/books/:id', (req, res) => {
+    const id = req.params.id;
+    const query = `SELECT * FROM books WHERE id = ${id}`;
+
+    conn.query(query, (err, result) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        const book = result[0];
+
+        res.render('book', { book });
+    });
+});
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', 
+    database: 'nodemysql',
+});
+
+app.get('/books/edit/:id', (req, res) => {
+    const id = req.params.id;
+
+    const query = `SELECT * FROM books WHERE id = ${id}`;
+
+    conn.query(query, (err, result) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        const book = result[0];
+
+        res.render('editbook', { book });
+    });
+});
+
+conn.connect(function(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Conectado ao banco de dados MySQL!');
+
+    app.listen(3000, () => {
+        console.log('Servidor rodando na porta 3000!');
+    });
+});
